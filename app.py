891761069c6f7766195fec42b2fca3475f3f1414
vidovic9 <mikkividovicc@gmail.com>
2025-06-19 17:06:52 +0200

    Add database connection, contact listing, and contact creation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv,find_dotenv
 import os
@@ -18,6 +18,40 @@ mysql = MySQL(app)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/test-db")
+def test_db():
+    try:
+        cur = mysql.connect.cursor()
+        cur.execute("SELECT 1")
+        return "Databse connected succesfully"
+    except Exception as e:
+        return f"Database connection failed: {str(e)}"
+
+@app.route("/contacts")
+def contacts():
+    cur = mysql.connect.cursor()
+    cur.execute("SELECT * FROM contacts")
+    data = cur.fetchall()
+    cur.close()
+    return render_template("contacts.html",contacts=data)
+
+@app.route("/add-contact",methods=['GET','POST'])
+def add_contact():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        cur = mysql.connect.cursor()
+        cur.execute("INSERT INTO contacts (name,email,phone) VALUES (%s,%s,%s)", (name,email,phone))
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('contacts'))
+    
+    return render_template("add_contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
